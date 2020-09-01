bc24dbd3a9716e49f1061743e9c9da38111231e5
AlexGarbus <alex.garbus@outlook.com>
2020-09-01 12:00:47 -0400

    Starting project

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraController : MonoBehaviour
+{
+    [Tooltip("The target object that this camera should follow.")]
+    [SerializeField] private Transform followTarget;
+    [Tooltip("The time it takes for this camera to smoothly move to its target.")]
+    [SerializeField] private float smoothTime;
+
+    private Vector2 dampVelocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get current position
+        Vector3 currentPosition = transform.position;
+
+        // Check whether current x and y positions are equal to target x and y positions
+        if ((Vector2)currentPosition != (Vector2)followTarget.position)
+        {
+            // Smoothly move to target
+            Vector2 dampPosition = Vector2.SmoothDamp(transform.position, followTarget.position, ref dampVelocity, smoothTime);
+            transform.position = new Vector3(dampPosition.x, dampPosition.y, currentPosition.z);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    [Tooltip("The velocity with which the player moves.")]
+    [SerializeField] private float moveVelocity;
+
+    private Rigidbody2D rigidbody2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Store a reference to the Rigidbody2D
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get input vector
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        // Set velocity
+        rigidbody2d.velocity = input * moveVelocity * Time.deltaTime;
+    }
+}
